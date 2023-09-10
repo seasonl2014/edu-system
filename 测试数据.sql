9333d5580f31d5d64e3fafae5eda5e55565c6548
马王天地 <11184629@qq.com>
2023-09-10 08:22:05 +0800

    上传数据库测试数据sql

---
+++ 测试数据.sql
@@ -0,0 +1,59 @@
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('1', '1', '2022-12-08 19:12:03.000000', '系统管理员', '1', '2022-12-08 19:11:59.000000', 'ROLE_ADMIN', '系统管理员', null, null, null, null, '');
+INSERT INTO `sys_role` VALUES ('2', '1', '2022-12-08 19:12:15.000000', '普通用户', '1', '2022-12-08 19:12:30.000000', 'ROLE_USER', '普通用户', null, null, null, null, '');
+
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', '1', '2022-12-02 12:55:05.000000', '我是一名管理员', '1', '2023-09-03 08:16:33.211500', 'E10ADC3949BA59ABBE56E057F20F883E', '系统管理员', '男', '1', 'admin', 'xueden2023@163.com', 'c66809ef-b6e6-43b9-a685-91915b57e3d6.jpg', '1', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('2', '1', '2022-12-05 13:06:48.040000', '韩立红颜知己，名义上的妾侍。落云宗女修，负责管理药园，韩立入落云宗修炼，归其管辖，被家族逼婚，一直推脱不肯完婚。后韩立结婴后，慕沛灵对外谎称已被韩立收为妾侍，韩立为突破瓶颈，假戏真唱收其为妾侍，改修颠凤培元功，后为突破元婴期未果身陨。', '1', '2022-12-06 03:36:51.824000', 'E10ADC3949BA59ABBE56E057F20F883E', '慕沛灵', '女', '1', 'mupeiling', 'mupeiling@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('3', '1', '2022-12-05 13:09:44.441000', '韩立红颜知己。又名雪玲，本是灵界银月妖狼玲珑仙子两魂之一。', '1', '2022-12-06 03:30:05.735000', 'E10ADC3949BA59ABBE56E057F20F883E', '银月', '女', '1', 'yinyue', 'yinyue@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('4', '1', '2022-12-05 13:10:42.589000', '这是一个凡人修仙者', '1', '2022-12-05 13:10:42.589000', 'E10ADC3949BA59ABBE56E057F20F883E', '韩立', '男', '1', 'hanli', '22@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('5', '1', '2022-12-05 13:14:47.104000', '韩立红颜知己。真名汪凝，原乱星海妙音门门主之女，韩立平生所遇第一美女、红颜知己。', '1', '2022-12-06 03:37:28.681000', 'E10ADC3949BA59ABBE56E057F20F883E', '紫灵', '女', '1', 'zilin', '33@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('6', '1', '2022-12-05 13:36:24.775000', '这是一个备注', '1', '2022-12-05 13:36:24.776000', 'E10ADC3949BA59ABBE56E057F20F883E', '厉飞雨', '男', '1', 'lifeiyu', '22@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('7', '1', '2022-12-05 13:38:54.366000', '韩立红颜知己。魁星岛附近的修仙小门派女修，对师姐妍丽重情重义，为已成鬼魂的师姐不死重生付出极大代价，与师姐修炼阴阳轮回诀被未知通道吸入灵界，后被姜老怪/青元子收为徒。韩立于其帮衬良多，对韩立情素暗生。', '1', '2022-12-05 13:38:54.366000', 'E10ADC3949BA59ABBE56E057F20F883E', '元瑶', '女', '1', 'yuanyao', '33@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('10', '1', '2022-12-06 01:13:56.953000', '韩立道侣。掩月宗女修，本命法宝朱雀环，修炼素女轮回功 。', '1', '2022-12-06 01:13:56.953000', 'E10ADC3949BA59ABBE56E057F20F883E', '南宫婉', '男', '1', 'nangongwan', '333@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('11', '1', '2022-12-06 03:46:19.410000', '韩立黄枫谷师姐。经过一些事与非后，爱上韩立，曾经向韩立告白，被一心求大道的韩立拒绝，为等韩立未婚不嫁，道消身死。后韩立在灵界碰到与巧倩长相极其相似之人，讶然不已。', '1', '2022-12-06 03:46:19.410000', 'E10ADC3949BA59ABBE56E057F20F883E', '陈巧倩', '女', '1', 'chenqiaoqian', 'chenqiaoqian@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('12', '1', '2022-12-06 03:47:02.119000', '韩立旧识。为人高傲，为了驻颜修炼化春决，喜欢男人为她争风吃醋，其师傅撮合她和韩立双修，两人相互不对眼，红拂派其和韩立一起参加燕家夺宝大会。', '1', '2022-12-06 03:47:02.119000', 'E10ADC3949BA59ABBE56E057F20F883E', '董萱儿', '女', '1', 'dongxuaner', 'dongxuaner@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('13', '1', '2022-12-06 03:47:40.257000', '韩立旧识。御灵宗女修，亲切可人，早年曾在太南小会上卖给韩立金竺符笔，多年后和柳眉等奉命追查至木灵婴下落，被韩立发现，韩立追上她时，发现是多年前旧识只将其打晕。', '1', '2022-12-06 03:47:40.258000', 'E10ADC3949BA59ABBE56E057F20F883E', '菡云芝', '女', '1', 'hanyunzhi', 'hanyunzhi@qq.com', null, '2', null, null, null, null, '');
+INSERT INTO `sys_user` VALUES ('14', '1', '2022-12-06 03:48:40.648000', '韩立旧识文樯之女。妙音门女修，外出执行危险任务，父女被韩立所救。韩立助其脱离妙音门文思月打算以身相许，奈何韩立不解风情，将她安置在一个荒岛上自行离去。', '1', '2022-12-06 03:48:40.648000', 'E10ADC3949BA59ABBE56E057F20F883E', '文思月', '男', '1', 'wensiyue', 'wensiyue@qq.com', null, '2', null, null, null, null, '');
+
+-- ----------------------------
+-- Records of edu_subject
+-- ----------------------------
+INSERT INTO `edu_subject` VALUES ('1', '基础', '0', '0', '2021-01-21 00:00:00.000000', '1', '2023-06-01 11:24:03.078802', '1', '1000233282', 'java基础课程', null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('2', '框架', '0', '0', '2021-01-21 00:00:00.000000', '1', '2023-06-01 11:24:20.177302', '1', '1000233283', 'java框架课程', null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('3', '微服务', '0', '0', '2021-01-21 00:00:00.000000', '1', '2023-06-01 11:24:32.640802', '1', '1000233299', '微服务课程', null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('5', '实战', '0', '0', '2021-01-21 00:00:00.000000', '1', '2023-06-01 11:24:46.784802', '1', '1000233300', 'java实战课程', null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('6', '数据库', '0', '0', '2021-01-21 00:00:00.000000', '1', '2023-06-01 11:24:59.881802', '1', '1000233302', '数据库课程', null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('7', '云原生', '0', '0', '2021-01-21 00:00:00.000000', '1', '2023-06-01 11:25:15.412802', '1', '1000233322', '云原生相关课程', null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('8', 'Java', '1', '0', '2021-01-21 21:14:21.000000', '1', '2021-01-21 19:38:21.000000', '1', '1000233619', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('9', 'boot+vue', '5', '0', '2021-01-21 21:15:41.000000', '1', '2023-05-02 09:58:45.000000', '1', '1000233620', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('11', 'springboot', '2', '0', '2021-01-21 21:35:38.000000', '1', '2021-01-21 19:38:29.000000', '1', '1000233621', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('12', '面试宝典', '1', '0', '2021-01-21 08:48:25.000000', '1', '2022-09-05 06:51:50.000000', '1', '1000233593', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('13', '算法', '1', '0', '2021-01-21 08:48:58.000000', '1', '2022-09-05 06:45:05.000000', '1', '1000233594', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('14', 'cloud+vue', '5', '0', '2021-01-21 08:49:31.000000', '1', '2021-01-21 19:39:06.000000', '1', '1000233595', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('15', 'ssm+vue', '5', '0', '2021-01-21 08:49:57.000000', '1', '2021-01-21 19:39:09.000000', '1', '1000233596', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('16', 'alibaba', '3', '0', '2021-01-21 08:50:25.000000', '1', '2021-01-21 19:38:50.000000', '1', '1000233597', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('17', 'docker', '7', '0', '2021-01-21 08:50:37.000000', '1', '2021-01-21 19:38:54.000000', '1', '1000233617', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('18', 'mysql', '6', '0', '2021-01-21 08:51:06.000000', '1', '2021-01-21 19:37:58.000000', '1', '1000233623', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('19', 'vue', '1', '0', '2021-01-21 08:51:31.000000', '1', '2021-01-21 19:38:01.000000', '1', '1000233625', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('20', 'kubernetes', '7', '0', '2021-01-21 08:51:51.000000', '1', '2021-01-21 19:37:27.000000', '1', '1000233627', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('21', '数据库', '7', '0', '2021-01-21 08:52:02.000000', '1', '2021-01-21 19:37:20.000000', '1', '1000233629', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('22', '开发工具', '1', '0', '2021-01-26 19:03:33.000000', '1', '2021-01-26 19:03:33.000000', '1', '1000235136', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('23', 'html5', '1', '0', '2021-01-29 19:41:58.000000', '1', '2021-01-29 19:41:58.000000', '1', '1000236281', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('24', 'css3', '1', '0', '2021-01-29 20:30:33.000000', '1', '2021-01-29 20:30:33.000000', '1', '1000236295', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('25', 'javascript', '1', '0', '2021-01-30 08:18:55.000000', '1', '2021-01-30 08:18:55.000000', '1', '1000236339', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('26', 'javaweb', '2', '0', '2021-01-30 08:55:56.000000', '1', '2021-01-30 08:55:56.000000', '1', '1000236342', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('27', 'redis', '6', '0', '2021-01-30 10:30:41.000000', '1', '2021-01-30 10:30:41.000000', '1', '1000236368', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('28', 'ssm', '2', '0', '2021-01-30 10:44:56.000000', '1', '2021-01-30 10:44:56.000000', '1', '1000236374', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('29', 'typescript', '1', '0', '2021-02-20 16:00:47.000000', '1', '2021-02-20 16:00:47.000000', '1', '1000250551', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('30', 'springcloud', '3', '0', '2021-03-22 17:48:26.000000', '1', '2021-03-22 17:48:26.000000', '1', '1000283665', null, null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('31', 'ssm+jsp', '5', '0', '2023-06-03 18:37:03.163886', '1', '2023-06-03 18:37:03.163891', '1', '1000488935', 'ssm+jsp整合开发项目', null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('32', 'ssm+extjs', '5', '0', '2023-06-05 16:39:02.626741', '1', '2023-06-05 16:39:02.626742', '1', '1000489123', 'ssm+extjs实战课程', null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('33', 'ssm+bootstrap', '5', '0', '2023-06-09 07:05:39.687193', '1', '2023-06-09 07:05:39.687197', '1', '1000490283', 'ssm+bootstrap实战项目', null, null, null, null, null);
+INSERT INTO `edu_subject` VALUES ('34', 'gin+vue', '5', '0', '2023-07-15 09:10:14.710198', '1', '2023-07-15 09:10:14.710202', '1', '1000500172', 'goweb实战', null, null, null, null, null);
